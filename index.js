77c42f1a61f8ac01466f39013135dc7ba1d43fb2
Harrowsville <harrowsville@bot.local>
2026-04-02 11:07:42 -0500

    Add forage command

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const COMMANDS = new Set([
   'barn', 'care', 'collect',
   'shop', 'buy', 'sell', 'sellall', 'selljunk',
   'expandfarm', 'give', 'donate', 'rename', 'gift',
-  'fund', 'leaderboard', 'profile', 'checkin',
+  'forage', 'fund', 'leaderboard', 'profile', 'checkin',
   'collection',
   'inventory', 'inv',
   'help',
