900cf08e8d9088b7c1e59ff1c92f8a99f71ce695
Harrowsville <harrowsville@bot.local>
2026-04-02 10:45:51 -0500

    Add rename command

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const COMMANDS = new Set([
   'fish', 'beachsearch', 'seafish',
   'barn', 'care', 'collect',
   'shop', 'buy', 'sell', 'sellall', 'selljunk',
-  'expandfarm', 'give', 'donate',
+  'expandfarm', 'give', 'donate', 'rename',
   'inventory', 'inv',
   'help',
 ]);
